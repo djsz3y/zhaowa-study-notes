4591a2c417e4ee37e4cd8ee4143e689cf30e021a
djsz3y.SnippetsLab.taylor <294764484@qq.com>
2023-05-21 03:48:06 +0800

    fix: 数组打平 对象打平

--- assault_course/突击课-算法-麓一/algorithm-1/5_flatten.js
+++ assault_course/突击课-算法-麓一/algorithm-1/5_flatten.js
@@ -2,7 +2,8 @@
 const arr = [1, 2, 3, [4, 5, [6, 7, 8], 9, 20]]
 
 function flattenArray(arr) {
-  if (!arr.length) return
+  // if (!arr.length) return
+  if (!Array.isArray(arr)) return
   return arr.reduce(
     (pre, cur) =>
       Array.isArray(cur) ? [...pre, ...flattenArray(cur)] : [...pre, cur],
@@ -31,10 +32,20 @@ const obj = {
 
 function flattenObject(obj) {
   if (typeof obj !== 'object' || obj == null) return
+  // let cost = 0 // 开销
   let res = {}
   const dfs = function (cur, prefix) {
+    // for (let k in cur) {
+    //   // cost++
+    //   if (typeof cur[k] === 'object' && cur[k] != null) {
+    //     dfs(cur[k], `${prefix}${prefix ? '.' : ''}${k}`)
+    //   } else {
+    //     res[`${prefix}.${k}`] = cur[k]
+    //   }
+    // }
     if (typeof cur === 'object' && cur != null) {
       for (let k in cur) {
+        // cost++
         dfs(cur[k], `${prefix}${prefix ? '.' : ''}${k}`)
       }
     } else {
@@ -42,7 +53,10 @@ function flattenObject(obj) {
     }
   }
   dfs(obj, '')
+  // console.log(cost)
   return res
 }
 
+// console.time('flattenObject')
 console.log(flattenObject(obj))
+// console.timeEnd('flattenObject')
