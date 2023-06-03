47dc88f0192339135b371404a18fa83781c2a9df
外险内健要做一个童蒙 <294764484@qq.com>
2023-06-03 15:31:11 +0800

    feat: ## 2.1 回溯方法论

---
+++ assault_course/algorithm-luyi/algorithm-3/backtrack.js
@@ -0,0 +1,21 @@
+var combine = function(n, k) {
+  const result = []
+  const path = []
+
+  function backtrack() {
+    if(condition) {
+      result.push([...path])
+      return
+    }
+
+    for() {
+      path.push()
+      backtrack()
+      path.pop()
+    }
+  }
+
+  backtrack()
+
+  return result
+}
